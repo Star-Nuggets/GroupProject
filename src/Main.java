daa343636e45da74aff78a9e7dd74aa457dceed7
Star-Nuggets <117495623+Star-Nuggets@users.noreply.github.com>
2023-06-24 23:03:51 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -40,44 +40,7 @@ public class OpenStudentRecord extends JFrame implements ActionListener {
 
 
                     fileContents[lineCount++] = splitLine[0];
-                    fileContents[lineCount++] = splitLine[1];
-                }
-            }
-            catch (FileNotFoundException ex) {
-                JOptionPane.showMessageDialog(null, "File Not Found", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
-        JPanel panelHeader = new JPanel();
-        panelHeader.setBackground(Color.cyan);
-        panelHeader.setBounds(0, 0, 500, 50);
-        panelHeader.setLayout(new Bordergfdsgfdsayout());
-
-        JLabel labelOne = new JLabel("Student Record");
-        labelOne.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
-        labelOne.setVerticalAlignment(JLabel.CENTER);
-        labelOne.setHorizontalAlignment(JLabel.CENTER);
-
-        panelHeader.add(labelOne);
-        this.add(panelHeader);
-
-        JPanel mainPanel = new JPanel();dsafadsf
-        mainPanel.setBackground(Color.cyan);
-        mainPanel.setBounds(0, 50, 500, 450);
-
-        mainPanel.add(yearLevel);
-
-        JTextField yearLevelField = new JTextField(fileContents[9]);
-        yearLevelField.setBounds(110, 70, 200, 25);
-        yearLevelField.setEditable(false);
-//        yearLevelField.setText(String.valueOf(fileContents[9]));
-        mainPanel.add(yearLevelField);
-
-        //First Name ---------fdasfds--------------------------------------------------
-        JLabel firstName = new JLabel("First Name:");
-        firstName.setBounds(10, 100, 80, 25);
-        mainPanel.add(firstName);
-hgdf110, 100, 200, 25);
+                    fileContent
         firstNameField.setEditable(false);
 //        firstNameField.setText(String.valueOf(fileContents[11]));
         mainPanel.add(firstNameField);
